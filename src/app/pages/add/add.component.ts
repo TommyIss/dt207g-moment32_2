aadfc53023d883454dba73d0bc8f723ce45447b0
Tommy Issa <tommyissa@hotmail.com>
2025-05-29 21:00:08 +0200

    Har skapat innehåll samt metoder för "Lägg till"-sida för att mata in samt lägga till nya inlägg i databasen

--- src/app/pages/add/add.component.ts
+++ src/app/pages/add/add.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { ExperiencesService } from '../../services/experiences.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Experience } from '../../models/experience';
 
 @Component({
   selector: 'app-add',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.css'
 })
 export class AddComponent {
+  // Properties
+  id: string = '';
+  companyname: string = '';
+  jobtitle: string = '';
+  location: string = '';
+  startdate: string = '';
+  enddate: string = '';
+  description: string = '';
+  error: string = '';
 
+  constructor( private experienceService: ExperiencesService, private router: Router) {}
+  
+  addData() {
+    let newExperience: Experience = {
+      id: this.id,
+      companyname: this.companyname,
+      jobtitle: this.jobtitle,
+      location: this.location,
+      startdate: this.startdate,
+      enddate: this.enddate,
+      description: this.description
+    };
+
+    this.experienceService.postData(newExperience).subscribe({
+      next: response => {
+        console.log(response);
+        this.router.navigate(['/']);
+      },
+      error: err => {
+        this.error = err.error.message;
+      }
+    });
+  }
 }
