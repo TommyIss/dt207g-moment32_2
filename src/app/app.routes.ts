366c6fd086814527794bf7732744e26d5ba0f75e
Tommy Issa <tommyissa@hotmail.com>
2025-05-29 19:37:10 +0200

    Har skapat komonenter för webbplatssidor och infogat dem till routes-fil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AddComponent } from './pages/add/add.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'add', component: AddComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
